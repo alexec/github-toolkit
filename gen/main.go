2263eb187410eeec3a4846db9bf6f96742d9f9bb
Alex Collins <alex_collins@intuit.com>
2019-10-02 15:58:11 -0700

    changes

--- gen/main.go
+++ gen/main.go
@@ -45,7 +45,7 @@ var rootCmd = &cobra.Command{
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Cards</title>
+    <title>Max %v Cards</title>
   </head>
   <body>
 <div class="container">
